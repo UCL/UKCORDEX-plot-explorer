99e03262f470cd0cbcb4e37425adb18537684293
Raquel Alegre <r.alegre@ucl.ac.uk>
2021-09-21 08:24:16 +0100

    Changed button label

--- src/components/PlotExplorerContainer.js
+++ src/components/PlotExplorerContainer.js
@@ -29,6 +29,8 @@ function PlotExplorerRow() {
     setDataRows([...dataRows, dataRow]);
   };
 
+
+
   const delDataRow = (index) => {
     console.log(index);
     console.log(dataRows);
@@ -38,15 +40,39 @@ function PlotExplorerRow() {
 
   return (
     <Container>
-      <OptionsRow
-        setPlotvar={setPlotvar}
-        setEvalagainst={setEvalagainst}
-        setRegmodel={setRegmodel}
-        setTimeperiod={setTimeperiod}
-      />
       {dataRows.map((row, index) => {
+        console.log(index);
+        if (index==0){
+          return (
+            <>
+              <OptionsRow
+                setPlotvar={setPlotvar}
+                setEvalagainst={setEvalagainst}
+                setRegmodel={setRegmodel}
+                setTimeperiod={setTimeperiod}
+                row={row}
+              />
+              <FigureRow
+                plotvar={row.plotvar}
+                evalagainst={row.evalagainst}
+                regmodel={row.regmodel}
+                timeperiod={row.timeperiod}
+              />
+            </>
+          );
+        }
+        else {
+
+        }
         return (
           <>
+            <OptionsRow
+              setPlotvar={setPlotvar}
+              setEvalagainst={setEvalagainst}
+              setRegmodel={setRegmodel}
+              setTimeperiod={setTimeperiod}
+              row={row}
+            />
             <FigureRow
               plotvar={row.plotvar}
               evalagainst={row.evalagainst}
@@ -58,7 +84,7 @@ function PlotExplorerRow() {
       })}
       <Row className="py-5">
         <Button onClick={() => addDataRow()} variant="outline-info">
-          Add another plot to compare
+          Show plot
         </Button>{" "}
       </Row>
     </Container>
