b6fce3266cd45f2be87e3fe1704fecd6c600cca7
Raquel Alegre <r.alegre@ucl.ac.uk>
2021-11-04 11:44:55 +0000

    Updated path to images according to new dropdown choices.

--- src/components/FigureRow.js
+++ src/components/FigureRow.js
@@ -3,14 +3,14 @@ import Row from 'react-bootstrap/Row';
 import Figure from 'react-bootstrap/Figure';
 import FigureImage from 'react-bootstrap/FigureImage';
 
-function FigureRow({plotvar, evalagainst, regmodel, timeperiod}){
+function FigureRow({plotvar, season, period, plottype}){
 
   return(
     <Row className="py-4">
        <Figure>
          <Figure.Image
            alt="Image NOT found"
-           src={`${process.env.PUBLIC_URL}/images/rmse-decomposition_${plotvar.value}_${evalagainst.value}_${regmodel.value}_${timeperiod.value}.png`}
+           src={`${process.env.PUBLIC_URL}/images/${plotvar.value}/${plottype.value}_${plotvar.value}_${season.value}_${period.value}.png`}
          />
        </Figure>
      </Row>
