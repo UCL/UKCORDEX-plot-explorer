aaea816bad1d532865c46689db6e2510c15b7b25
Raquel Alegre <r.alegre@ucl.ac.uk>
2021-11-05 16:41:52 +0000

    Only show the image if user has made selection.

--- src/components/FigureRow.js
+++ src/components/FigureRow.js
@@ -5,16 +5,22 @@ import FigureImage from 'react-bootstrap/FigureImage';
 
 function FigureRow({plotvar, season, period, plottype}){
 
-  return(
-    <Row className="py-4">
-       <Figure>
-         <Figure.Image
-           alt="Image NOT found"
-           src={`${process.env.PUBLIC_URL}/images/${plotvar.value}/${plottype.value}_${plotvar.value}_${season.value}_${period.value}.png`}
-         />
-       </Figure>
-     </Row>
-  );
+  if ([plotvar, season, period, plottype].some(el => el === null)) {
+    console.log("error, you didnt make enough selections");
+    return(null);
+  }
+  else {
+    return(
+      <Row className="py-4">
+         <Figure>
+           <Figure.Image
+             alt="Image NOT found"
+             src={`${process.env.PUBLIC_URL}/images/${plotvar.value}/${plottype.value}_${plotvar.value}_${season.value}_${period.value}.png`}
+           />
+         </Figure>
+       </Row>
+    );
+  }
 };
 
 export default FigureRow;
