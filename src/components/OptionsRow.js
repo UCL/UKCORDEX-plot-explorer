f827f0f1120dea83d2f2378feacd70212ebcfdb7
Raquel Alegre <r.alegre@ucl.ac.uk>
2021-11-09 08:15:02 +0000

    Updated indices labels with their ID.

--- src/components/OptionsRow.js
+++ src/components/OptionsRow.js
@@ -5,26 +5,26 @@ import Col from 'react-bootstrap/Col';
 
 // Some of these might not be relevant anymore
 const plotvarOptions = [
-  { value: 'fwd', label: 'Surface Snow Amount' },
-  { value: 'tas', label: 'Daily Near-Surface Air Temperature' },
-  { value: 'tasmax', label: 'Daily Maximum Near-Surface Air Temperature' },
-  { value: 'tasmin', label: 'Daily Minimum Near-Surface Air Temperature' },
-  { value: 'tas99', label: 'Daily Near-Surface Air Temperature (Percentile 99)' },
-  { value: 'tas01', label: 'Daily Near-Surface Air Temperature (Percenrile 1)' },
-  { value: 'pr', label: 'Precipitation' },
-  { value: 'r99ptot', label: 'r99ptot' },
-  { value: 'sfcWind', label: 'Near-Surface Wind Speed' },
-  { value: 'rss', label: 'Net surface long wave flux' },
-  { value: 'clt', label: 'Total Cloud Fraction' },
-  { value: 'prc', label: 'Convective Precipitation' },
-  { value: 'tas', label: 'Near-Surface Air Temperature' },
-  { value: 'wsgsmax', label: 'Daily Maximum Near-Surface Wind Speed of Gust' },
-  { value: 'psl', label: 'Sea Level Pressure' },
-  { value: 'hurs', label: 'Near-Surface Relative humidity' },
-  { value: 'uas', label: 'Eastward Near-Surface Wind' },
-  { value: 'vas', label: 'Northward Near-Surface Wind ' },
-  { value: 'prsn', label: 'Snowfall Flux' },
-  { value: 'rls', label: 'Net surface long wave flux' }
+  { value: 'fwd', label: 'fwd - Surface Snow Amount' },
+  { value: 'tas', label: 'tas - Daily Near-Surface Air Temperature' },
+  { value: 'tasmax', label: 'tasmax - Daily Maximum Near-Surface Air Temperature' },
+  { value: 'tasmin', label: 'tasmin - Daily Minimum Near-Surface Air Temperature' },
+  { value: 'tas99', label: 'tas99 - Daily Near-Surface Air Temperature (Percentile 99)' },
+  { value: 'tas01', label: 'tas01 - Daily Near-Surface Air Temperature (Percenrile 1)' },
+  { value: 'pr', label: 'pr - Precipitation' },
+  { value: 'r99ptot', label: 'r99ptot - r99ptot' },
+  { value: 'sfcWind', label: 'sfcWind - Near-Surface Wind Speed' },
+  { value: 'rss', label: 'rss - Net surface long wave flux' },
+  { value: 'clt', label: 'clt - Total Cloud Fraction' },
+  { value: 'prc', label: 'prc - Convective Precipitation' },
+  { value: 'tas', label: 'tas - Near-Surface Air Temperature' },
+  { value: 'wsgsmax', label: 'wsgsmax - Daily Maximum Near-Surface Wind Speed of Gust' },
+  { value: 'psl', label: 'psl - Sea Level Pressure' },
+  { value: 'hurs', label: 'hurs - Near-Surface Relative humidity' },
+  { value: 'uas', label: 'uas - Eastward Near-Surface Wind' },
+  { value: 'vas', label: 'vas - Northward Near-Surface Wind ' },
+  { value: 'prsn', label: 'prsn - Snowfall Flux' },
+  { value: 'rls', label: 'rls - Net surface long wave flux' }
 ];
 
 // TODO: This list will be updated as more plots are generated. It covers ~100
