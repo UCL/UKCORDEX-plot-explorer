8d166904f362c03547be74ac245d72fcdbf3a2c4
Raquel Alegre <r.alegre@ucl.ac.uk>
2021-11-10 19:08:49 +0000

    Correctly concatenate string for 'image not found' message.

--- src/components/FigureRow.js
+++ src/components/FigureRow.js
@@ -29,7 +29,7 @@ function FigureRow({plotvars, seasons, periods, plottypes}){
                 <Row className="py-4">
                   <Figure>
                     <FigureImage
-                     alt="Image not found for ${plotvar.value} ${plottype.value} ${season.value} ${period.value}"
+                     href={`Image not found for ${plotvar.value} ${plottype.value} ${season.value} ${period.value}`}
                      src={`${process.env.PUBLIC_URL}/images/${plotvar.value}/${plottype.value}_${plotvar.value}_${season.value}_${period.value}.png`}
                     />
                   </Figure>
